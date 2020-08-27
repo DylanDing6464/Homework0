cfc24e20f955f3f6261c34d7732a2f27853cd5f8
dyalnding <dylanding6464@gmail.com>
2020-08-27 06:04:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+celsiusDegree= input('Enter temperature in celsuis: \n')
+celsiusDegree = int(celsiusDegree)
+
+fahrenheitDegree = 1.8*celsiusDegree+32
+
+print(f"{celsiusDegree}° in celsius is equivalent to {fahrenheitDegree}° Fahrenheit ")
