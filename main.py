8181cf2b8427d1f36dbb659529718a3159d7b6ae
dyalnding <dylanding6464@gmail.com>
2020-08-27 06:17:39 +0000

    Calculation changing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 celsiusDegree= input('Enter temperature in celsuis: ')
 celsiusDegree = float(celsiusDegree)
 
-fahrenheitDegree = 1.8*celsiusDegree+32
+fahrenheitDegree = (9/5)*celsiusDegree+32
 
 print(f"{celsiusDegree}° in Celsius is equivalent to {fahrenheitDegree}° Fahrenheit.")
